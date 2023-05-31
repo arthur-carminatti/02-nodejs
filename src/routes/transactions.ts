f46e379058d964881fbce619577d8d7b127b555e
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-05-31 18:11:08 -0300

    plugins fastify

---
+++ src/routes/transactions.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from "fastify"
+import { knex } from "../database"
+
+export async function transactionsRoutes(app: FastifyInstance) {
+    app.get('/hello', async () => {
+        const transaction = await knex('transacitons').insert({
+            id: crypto.randomUUID(),
+            title: 'Transação de teste',
+            amount: 1000,
+        }).returning('*')
+
+        return transaction
+    })
+}
