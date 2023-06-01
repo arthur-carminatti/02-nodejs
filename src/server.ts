d79cbf73e976d72cf695362172443e214a3aefcb
arthur-carminatti <arthurcarminattigit@gmail.com>
2023-06-01 10:20:42 -0300

    Create transactions

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,12 @@ import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+    prefix: 'transactions'
+})
 
 app.listen({
     port: env.PORT,
 }).then(() => {
     console.log('HTTP Server Running!')
 })
-
-//test
